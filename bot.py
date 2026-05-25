27e42383fe4a1a9b3dd5ea6d31fc3a5e1346bf5d
tripanboi-png <tripanboi@gmail.com>
2026-05-25 23:38:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ from config import (
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            session_name="Bot",
+            name="Bot",
             api_hash=API_HASH,
             api_id=APP_ID,
             plugins={"root": "plugins"},
